aaffb261ec5863321f7612859c2c7fbb0189f878
EvanSpeciale <speciale@oregonstate.edu>
2020-07-01 22:23:16 -0700

    added navbar

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+        <Container className='bg-dark'>
+
+        </Container>
+        );
+    }
+}
+
+export default HomePage;
