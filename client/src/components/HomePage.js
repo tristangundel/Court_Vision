b41ac25bf20fc8be53736fb2a2c415028e1139d7
Andres Arizala <arizala13@gmail.com>
2020-07-11 21:45:10 -0500

    updating

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,16 +1,58 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../App.css';
+import React from "react";
+import { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
 
-class HomePage extends React.Component {
-    render() {
-        return (
-        <Container className='bg-dark'>
+class ExampleTeam extends Component {
+  constructor(props) {
+    super(props);
 
-        </Container>
-        );
-    }
+    this.state = {
+      isLoading: true,
+      shortName: "",
+      nickName: "",
+      city: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("/get-houston")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.data.cod === "404") {
+          this.setState({
+            isLoading: false,
+            cityNotFound: "404",
+          });
+        } else {
+          // Determine weather icon
+          this.setState({
+            isLoading: false,
+            shortName: data.data.shortName,
+            nickName: data.nickName,
+            city: data.city,
+            cityName: data.data.name,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const DisplayTeam = (
+      <div>
+        <p>This is the city: {this.state.city}</p>
+        <p>This is the shortName: {this.state.shortName}</p>
+        <p>This is the nickName: {this.state.nickName}</p>
+      </div>
+    );
+
+    const currentTeam = <div> {DisplayTeam} </div>;
+
+    return <div>{currentTeam}</div>;
+  }
 }
 
-export default HomePage;
+export default ExampleTeam;
