8cbf008d585b6eaadef9a7c5626abbd1260a92a5
Andres Arizala <arizala13@gmail.com>
2020-07-12 20:31:17 -0500

    WIP with displaying data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,10 +3,43 @@ import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 
-class HomePage extends React.Component {
-  render() {
-    return <Container className='bg-dark'></Container>;
-  }
-}
+// class HomePage extends React.Component {
+//   render() {
+//     return <Container className='bg-dark'></Container>;
+//   }
+// }
 
-export default HomePage;
+// export default HomePage;
+
+import api from "./teams";
+
+const IndexPage = () => {
+  // Create state variables
+  let [responseData, setResponseData] = React.useState("");
+  // fetches data
+  const fetchData = (e) => {
+    e.preventDefault();
+    api
+      .getData()
+      .then((response) => {
+        setResponseData(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <h1>{responseData.title}</h1>
+      <button onClick={(e) => fetchData(e)} type='button'>
+        Click Me For Data
+      </button>
+      {responseData.dates &&
+        responseData.dates.map((date) => {
+          return <p>{date}</p>;
+        })}
+    </div>
+  );
+};
+export default IndexPage;
