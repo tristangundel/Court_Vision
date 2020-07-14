300a8530f4a5afb70b57d62852e320a46b27fcc9
Andres Arizala <arizala13@gmail.com>
2020-07-13 19:52:59 -0500

    sync with master

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -29,35 +29,4 @@ class HomePage extends React.Component {
   }
 }
 
-import api from "./teams";
-
-const IndexPage = () => {
-  // Create state variables
-  let [responseData, setResponseData] = React.useState("");
-  // fetches data
-  const fetchData = (e) => {
-    e.preventDefault();
-    api
-      .getData()
-      .then((response) => {
-        setResponseData(response.data);
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  return (
-    <div>
-      <h1>{responseData.title}</h1>
-      <button onClick={(e) => fetchData(e)} type='button'>
-        Click Me For Data
-      </button>
-      {responseData.dates &&
-        responseData.dates.map((date) => {
-          return <p>{date}</p>;
-        })}
-    </div>
-  );
-};
-export default IndexPage;
+export default HomePage;
