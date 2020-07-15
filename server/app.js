1c2602d33aba7ff41d52dfd1191485e53a33b5ff
Andres Arizala <arizala13@gmail.com>
2020-07-14 21:04:52 -0500

    updates to clean up

--- server/app.js
+++ server/app.js
@@ -2,17 +2,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const config = require("config");
-var bodyParser = require("body-parser");
-const axios = require("axios");
 
 // assign variables for port and application's server
 const app = express();
 const port = process.env.port || 5000;
 
 connectDB();
-
-// tells routes to use this
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // init middleware
 app.use(express.json({ extended: false }));
@@ -31,25 +26,4 @@ app.use("/api/teams", require("./routes/api/teams"));
 
 app.listen(port, function () {
   console.log("Server started: Listening on port " + port + ".");
-});
-
-app.get("/get-houston", (req, res) => {
-  const axios = require("axios");
-
-  axios({
-    method: "GET",
-    url: "https://api-nba-v1.p.rapidapi.com/teams/city/Houston",
-    headers: {
-      "content-type": "application/octet-stream",
-      "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-      "x-rapidapi-key": config.get("RAPIDAPI_KEY"),
-      useQueryString: true,
-    },
-  })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
 });
