8a844691df90ce3619d26958d08f5cb1904f4d0d
Andres Arizala <arizala13@gmail.com>
2020-07-14 19:35:17 -0500

    Synced with master from July 14 1:00 PM CST

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,17 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import {setCurrentUser} from './redux/actions/authActions';
-import store from './redux/store';
-import './App.css';
-import NavBar from './components/NavBar';
-import HomePage from './components/HomePage';
+import { setCurrentUser } from "./redux/actions/authActions";
+import store from "./redux/store";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
 // import Footer from './components/Footer';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Player from './components/Player';
-import Team from './components/Team';
-import LeagueLeaders from './components/LeagueLeaders';
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Player from "./components/Player";
+import Team from "./components/Team";
+import LeagueLeaders from "./components/LeagueLeaders";
 
 function App() {
   // set current user if token exists in redux store
@@ -25,19 +25,18 @@ function App() {
     store.dispatch(setCurrentUser(decoded));
   }
 
-    return ( 
-      <Provider store={store}>
-        <Router>
-          <div className = 'App' >
-            <NavBar />
-            <Route exact path='/' component={ HomePage } />
-            <div id='container'>
-              <Route exact path='/register' component={ Register } />
-              <Route exact path='/login' component={ Login } />
-              <Route exact path='/testplayer' component= { Player } />
-              <Route exact path='/team/:teamId' component= { Team } />
-              <Route exact path='/leagueleaders' component= {LeagueLeaders} />
-            </div>
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className='App'>
+          <NavBar />
+          <Route exact path='/' component={HomePage} />
+          <div id='container'>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/testplayer' component={Player} />
+            <Route exact path='/team/:teamId' component={Team} />
+            <Route exact path='/leagueleaders' component={LeagueLeaders} />
           </div>
         </div>
       </Router>
