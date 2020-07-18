8494c224005f0a3c9e9f6144a69d45219ca63b0a
Tristan <tristangundel@gmail.com>
2020-07-18 11:25:42 -0700

    production ready branch with all master branch changes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // declare variables for dependencies
 const express = require("express");
 const connectDB = require("./config/db");
-const config = require("config");
 
 // assign variables for port and application's server
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 // init middleware
 app.use(express.json({ extended: false }));
 app.use(express.static('./utils'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Define routes
 app.use("/api/users", require("./routes/api/users"));
@@ -20,6 +20,16 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/players", require("./routes/api/players"));
 app.use("/api/teams", require("./routes/api/teams"));
+
+if (process.env.NODE_ENV === 'production') {
+  // serve static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // handle react routing
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index/html'));
+  });
+}
 
 app.listen(port, function () {
   console.log("Server started: Listening on port " + port + ".");
