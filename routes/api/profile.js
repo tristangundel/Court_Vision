d2e6743f0bc852e28882062752ab774a927f02f2
Andres Arizala <arizala13@gmail.com>
2020-07-19 10:00:34 -0500

    working on profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-// const auth = require("../../middleware/auth");
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/Users");
@@ -10,7 +11,7 @@ const User = require("../../models/Users");
 // @access      private - needs token
 
 // asnyc await used because we are using mongoos promise
-router.get("/me", async (req, res) => {
+router.get("/me", auth, async (req, res) => {
   try {
     // get the user from user profile
     profile = await Profile.findOne({ user: req.user.id }).populate(
@@ -30,5 +31,46 @@ router.get("/me", async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @routeGET    Post api/profile/me
+// @desc        create or update a user profile
+// @access      Private - needs token
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills are required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validtionResult(req);
+
+    //check for errors
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (team) profileFields.team = team;
+    if (status) profileFields.website = status;
+    if (location) profileFields.location = location;
+    if (youtube) profileFields.youtube = youtube;
+    if (twitter) profileFields.twitter = twitter;
+    if (instagram) profileFields.instagram = instagram;
+
+    // We can use an array to save favorite players
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    console.log(skills);
+
+    res.send("Hello");
+  }
+);
 
 module.exports = router;
