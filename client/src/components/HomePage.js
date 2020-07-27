8924ee38524d9b8345ab98c61664c9186de3b3f3
Tristan <tristangundel@gmail.com>
2020-07-27 11:25:33 -0700

    added dynamic homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,32 +1,49 @@
 import React from "react";
 // import { Container, Col, Row } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+import { Link, Redirect, withRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 
 class HomePage extends React.Component {
   render() {
-    return (
-      <div className='landing'>
-        <div className='dark-overlay landing-inner text-light'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-12 text-center'>
-                <h1 className='display-3 mb-4'>CourtVision</h1>
-                <hr />
-                <Link to='register' className='btn btn-lg btn-info mr-2'>
-                  Sign Up
-                </Link>
-                <Link to='login' className='btn btn-lg btn-light'>
-                  Login
-                </Link>
+    // retrieve whether or not user is logged in
+    const { isAuthenticated } = this.props.auth;
+
+    if (isAuthenticated) {
+      return (<Redirect to="/dashboard" />);
+    } else {
+      return (
+        <div className='landing'>
+          <div className='dark-overlay landing-inner text-light'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-md-12 text-center'>
+                  <h1 className='display-3 mb-4'>CourtVision</h1>
+                  <hr />
+                  <Link to='register' className='btn btn-lg btn-info mr-2'>
+                    Sign Up
+                  </Link>
+                  <Link to='login' className='btn btn-lg btn-light'>
+                    Login
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
-export default HomePage;
+HomePage.propTypes = {
+  auth: PropTypes.object
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(withRouter(HomePage) )
