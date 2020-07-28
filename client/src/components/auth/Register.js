e11eacc433bad7cd545a556d341d4e4fea3d28d3
Andres Arizala <arizala13@gmail.com>
2020-07-27 22:00:56 -0500

    going back to previous setup -- still issues with getting profile

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,90 +1,124 @@
+import React from "react";
 import { connect } from "react-redux";
-import React, { Fragment, useState } from "react";
 import { PropTypes } from "prop-types";
-import { Link, Redirect } from "react-router-dom";
-// import { withRouter, Link, Redirect } from "react-router-dom";
-// import { Container, Row, Form, Button } from "react-bootstrap";
-// import { GiBasketballBall } from "react-icons/gi";
+import { withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Container, Row, Form, Button } from "react-bootstrap";
+import { GiBasketballBall } from "react-icons/gi";
 import { registerUser } from "../../redux/actions/authActions";
-import { setAlert } from "../../redux/actions/alert";
+// import { GET_ERRORS } from "../../redux/actions/actionTypes";
 
-const Register = ({ setAlert, registerUser, isAuthenticated }) => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    password2: "",
-  });
-
-  const { name, email, password, password2 } = formData;
-
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    if (password !== password2) {
-      setAlert("Passwords do not match", "danger");
-    } else {
-      registerUser({ name, email, password });
-      console.log(formData);
-    }
-  };
-
-  if (isAuthenticated) {
-    return <Redirect to='/dashboard' />;
+class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      name: "",
+      password: "",
+      confirmPass: "",
+      errors: {},
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  return (
-    <Fragment>
-      <h1 className='large text-primary'>Sign Up</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Create Your Account
-      </p>
-      <form className='form' onSubmit={onSubmit}>
-        <div className='form-group'>
-          <input
-            type='text'
-            placeholder='Name'
-            name='name'
-            value={name}
-            onChange={onChange}
-          />
+
+  // Upon updating component, errors will be transferred to state
+  componentDidUpdate(nextProps) {
+    if (
+      !(Object.keys(nextProps.errors).length === 0) &&
+      typeof nextProps.errors === "object"
+    ) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  // handles changes in the input values when a user types
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  // handles submission of form to create a new user
+  handleSubmit(event) {
+    event.preventDefault();
+    const newUser = {
+      email: this.state.email,
+      name: this.state.name,
+      password: this.state.password,
+      confirmPass: this.state.confirmPass,
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+  }
+
+  render() {
+    return (
+      <Container>
+        <div className='authForm'>
+          <Row className='justify-content-center my-4'>
+            <h1 className='d-block-inline'>
+              CourtVision{" "}
+              <GiBasketballBall
+                size={48}
+                className='d-block-inline align-top'
+              />
+            </h1>
+          </Row>
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId='formGroupEmail'>
+              <Form.Control
+                type='email'
+                placeholder='Email'
+                name='email'
+                value={this.state.email}
+                onChange={this.handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId='formGroupName'>
+              <Form.Control
+                type='text'
+                placeholder='Name'
+                name='name'
+                value={this.state.name}
+                onChange={this.handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId='formGroupPassword'>
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                name='password'
+                value={this.state.password}
+                onChange={this.handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId='formGroupConfirmPassword'>
+              <Form.Control
+                type='password'
+                placeholder='Confirm Password'
+                name='confirmPass'
+                value={this.state.confirmPass}
+                onChange={this.handleChange}
+                required
+              />
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              Register
+            </Button>
+          </Form>
+          <p className='mx-4 my-3'>
+            Already have an account? <Link to='/login'>Log in Here</Link>
+          </p>
         </div>
-        <div className='form-group'>
-          <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
-            value={email}
-            onChange={onChange}
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Password'
-            name='password'
-            value={password}
-            onChange={onChange}
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Confirm Password'
-            name='password2'
-            value={password2}
-            onChange={onChange}
-          />
-        </div>
-        <input type='submit' className='btn btn-primary' value='Register' />
-      </form>
-      <p className='my-1'>
-        Already have an account? <Link to='/login'>Sign In</Link>
-      </p>
-    </Fragment>
-  );
-};
+      </Container>
+    );
+  }
+}
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
@@ -97,4 +131,4 @@ const mapStateToProps = (state) => ({
   errors: state.errors,
 });
 
-export default connect(mapStateToProps, { setAlert, registerUser })(Register);
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -5,6 +5,7 @@ import { GET_PROFILE, PROFILE_ERROR } from "./actionTypes";
 // make request for backend to get User profiles
 export const getCurrentProfile = () => async (dispatch) => {
   try {
+    console.log(axios.defaults.headers.common["x-auth-token"]);
     const res = await axios.get("/api/profile/me");
 
     dispatch({
