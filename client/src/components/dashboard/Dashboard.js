0ad9515d1481ebbea13eaf2cc861f8dccad2590b
Andres Arizala <arizala13@gmail.com>
2020-07-27 22:46:27 -0500

    dashboard still WIP

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,14 +3,21 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../redux/actions/profile";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user } }) => {
   useEffect(() => {
     getCurrentProfile();
+    console.log(user);
   }, [getCurrentProfile]);
 
   return (
     <Fragment>
-      <h1>This is where the Dashboard will be</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Welcome {user && user._id}
+      </p>
+      <h1>This is the Dashboard </h1>
+      <h4> Favorite players </h4>
+      <h4> Favorite teams </h4>
+      <h4> Status </h4>
     </Fragment>
   );
 };
