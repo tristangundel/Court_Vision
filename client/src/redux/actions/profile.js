c6b8b4630a5abcce0e6a75304301124882352e68
Andres Arizala <arizala13@gmail.com>
2020-07-30 15:16:45 -0500

    dashboard shows correct data

--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -8,9 +8,9 @@ export const getCurrentProfile = (user) => async (dispatch) => {
     const get_user = url + user;
     const res = await axios.get(get_user);
 
-    console.log("This is the const url", url);
-    console.log("This is the const user", user);
-    console.log("This is the combined url", get_user);
+    // console.log("This is the const url", url);
+    // console.log("This is the const user", user);
+    // console.log("This is the combined url", get_user);
 
     console.log("in profile.js after await");
     console.log("This is the user", user);
