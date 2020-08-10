cc4eca83506582a4279c1586969366da5e929de8
Andres Arizala <arizala13@gmail.com>
2020-08-10 18:33:54 -0500

    adding some more basic tests

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -8,4 +8,18 @@ describe("My First Test", function () {
   });
 });
 
+describe("My Second Test", function () {
+  it("Go to Login from homepage", function () {
+    cy.visit("http://localhost:3000");
+
+    cy.contains("Login").click();
+
+    cy.url().should("include", "/login");
+
+    // cy.get(".email")
+    //   .type("fakeemail@email.com")
+    //   .should("have.value", "fakeemail@email.com");
+  });
+});
+
 // need to make the above work with our website to test
